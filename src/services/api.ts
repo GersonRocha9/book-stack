d73d5d5cf9d79bef2395fae5346aabe890e90522
Gerson Rocha <gersonrocha9@gmail.com>
2022-10-31 13:36:17 -0300

    feat: pass user and password to API requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://lazy-gold-pigeon-coat.cyclic.app",
+
+  auth: {
+    username: "prod",
+    password: "pO655d&E8i",
+  },
 });
 
 export default api;
